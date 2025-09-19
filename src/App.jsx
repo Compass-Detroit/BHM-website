5c4242d0ac104d7572be9b3010bf4ac9962b331d
Shugmi Shumunov <SShumunov@gmail.com>
2025-09-19 13:28:05 -0400

    refactor: Create route for previous event years and add prior year data
    - Create a simple layout component with the NavBar & Footer
    - Update 2023 speakers time to work properly with `date-fns` dependency
    - Add comments to the Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 import Home from '@/pages/Home'
+import PreviousEvents from '@/pages/PreviousEvents'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/previous-events" element={<PreviousEvents />} />
       </Routes>
     </Router>
   )
