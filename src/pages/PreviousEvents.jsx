844dba308d557beb47b986d2d8c19df043f1f0ca
Greg Miller <hello@gregmiller.io>
2026-02-18 11:55:38 -0500

    refactor: functionality + content
    - create illusion that page is scrolling into place
    - page is fading in
    - add content for other events in the series
    - refactor content organization
    - add links to sites online for more info

--- src/pages/PreviousEvents.jsx
+++ src/pages/PreviousEvents.jsx
@@ -12,8 +12,8 @@ const PreviousEvents = () => {
     <PageLayout>
       <section
         aria-labelledby="main-heading"
-        className={`mt-16 bg-primary-100 transition-opacity duration-500 ease-out ${
-          isVisible ? 'opacity-100' : 'opacity-0'
+        className={`bg-inherit pt-72 transition-[opacity,transform] duration-1000 ease-out ${
+          isVisible ? '-translate-y-56 opacity-100' : 'translate-y-24 opacity-0'
         }`}
       >
         <div className="flex flex-col items-center justify-center px-8 py-12 sm:px-10 sm:py-16 md:px-14 md:py-20 lg:px-16 lg:py-24">
@@ -24,12 +24,12 @@ const PreviousEvents = () => {
             Compass Detroit Past Events
           </h1>
 
-          <p className="mx-auto w-5/6 text-lg leading-relaxed text-primary-950 lg:w-2/3">
+          <p className="mx-auto w-5/6 text-2xl leading-relaxed text-primary-950 lg:w-2/3">
             Compass Detroit is a community-driven event connecting tech
             enthusiasts, professionals, and students to network and explore
-            industry trends. This summit celebrates Black contributions to
-            technology and emphasizes the necessity of diversity within the
-            field.
+            industry trends. Our organization, Compass, stands for the
+            collective of minority professionals and STEAM societies in
+            Michigan.
           </p>
 
           <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-primary-950 lg:w-2/3">
@@ -37,30 +37,64 @@ const PreviousEvents = () => {
             talent and fosters a tradition of collaborative learning.
           </p>
 
-          <h2 className="mt-8 text-center font-biorhyme text-5xl text-bhm-neutral-900 md:text-5xl lg:text-6xl">
-            Black History Month Tech Summit
+          <h2 className="mt-8 text-center font-biorhyme text-4xl font-bold text-bhm-neutral-700">
+            Black History Month: Innovation Summit
           </h2>
-          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-primary-950 lg:w-2/3">
-            The Black History Month Tech Summit connects tech enthusiasts,
-            professionals, and students to network and explore industry trends.
-            This event celebrates Black contributions to technology and
-            reinforces the importance of diversity in the field.
+          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-bhm-neutral-700 lg:w-2/3">
+            The{' '}
+            <a
+              href="https://bhmtechsummit.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-bold text-primary-800 underline hover:text-primary-600"
+            >
+              Black History Month Innovation Summit
+            </a>{' '}
+            connects tech enthusiasts, professionals, and students to network
+            and explore industry trends. This event celebrates Black
+            contributions to technology and reinforces the importance of
+            diversity in the field.
           </p>
-          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-primary-950 lg:w-2/3">
+          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-bhm-neutral-700 lg:w-2/3">
             Through expert speakers and workshops, the summit highlights local
             talent and continues a tradition of collaborative learning.
           </p>
 
-          <h2 className="mt-8 text-center font-biorhyme text-5xl text-bhm-neutral-900 md:text-5xl lg:text-6xl">
+          <h2 className="mt-8 text-center font-biorhyme text-4xl font-bold text-bhm-neutral-700">
+            Women Techmakers&apos;: International Women&apos;s Day
+          </h2>
+          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-bhm-neutral-700 lg:w-2/3">
+            The{' '}
+            <a
+              href="https://gdg.community.dev/iwd/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="font-bold text-primary-800 underline hover:text-primary-600"
+            >
+              Women Techmakers&apos; International Women&apos;s Day
+            </a>{' '}
+            connects tech enthusiasts, professionals, and students to network
+            and explore industry trends. This event celebrates International
+            Women&apos;s Day and reinforces the importance of diversity in the
+            field.
+          </p>
+          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-bhm-neutral-700 lg:w-2/3">
+            Through expert speakers and workshops, the summit highlights local
+            talent and continues a tradition of collaborative learning. Through
+            expert speakers and workshops, the summit highlights local talent
+            and continues a tradition of collaborative learning.
+          </p>
+
+          <h2 className="mt-8 text-center font-biorhyme text-4xl font-bold text-bhm-neutral-700">
             Michigan DevFest
           </h2>
-          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-primary-950 lg:w-2/3">
+          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-bhm-neutral-700 lg:w-2/3">
             Hosted by GDG-Detroit for over a decade,{' '}
             <a
               href="https://midevfest.com"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-primary-800 underline hover:text-primary-600"
+              className="font-bold text-primary-800 underline hover:text-primary-600"
             >
               Michigan DevFest
             </a>{' '}
@@ -69,7 +103,7 @@ const PreviousEvents = () => {
             into a statewide draw, offering diverse workshops and expert-led
             sessions.
           </p>
-          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-primary-950 lg:w-2/3">
+          <p className="mx-auto mt-4 w-5/6 text-lg leading-relaxed text-bhm-neutral-700 lg:w-2/3">
             As a long-standing member of the Compass network, DevFest highlights
             the immense talent within our local tech ecosystem and continues a
             tradition of collaborative learning.
